1202098064772e5c84b51ccadb34ad6a174d2088
aishsingh <a@aishsingh.com>
2016-12-13 18:26:11 +1100

    Removed incomplete unitests

--- jni/src/unittests/physics_test.cc
+++ jni/src/unittests/physics_test.cc
@@ -38,39 +38,39 @@ TEST(splitValueFromAngleTest, Value1Angle0) {
     EXPECT_EQ(hori, 0);
     EXPECT_EQ(vert, 50);
 }
-TEST(splitValueFromAngleTest, Value1Angle90) {
-    float hori = 0;
-    float vert = 0;
-
-    Physics::splitValueFromAngle(50, 90, &hori, &vert);
-
-    EXPECT_EQ(hori, 12.5);
-    EXPECT_EQ(vert, 37.5);
-}
-TEST(splitValueFromAngleTest, Value1Angle180) {
-    float hori = 0;
-    float vert = 0;
-
-    Physics::splitValueFromAngle(50, 180, &hori, &vert);
-
-    EXPECT_EQ(hori, 25);
-    EXPECT_EQ(vert, 25);
-}
-TEST(splitValueFromAngleTest, Value1Angle270) {
-    float hori = 0;
-    float vert = 0;
-
-    Physics::splitValueFromAngle(50, 270, &hori, &vert);
-
-    EXPECT_EQ(hori, 37.5);
-    EXPECT_EQ(vert, 12.5);
-}
-TEST(splitValueFromAngleTest, Value1Angle360) {
-    float hori = 0;
-    float vert = 0;
-
-    Physics::splitValueFromAngle(50, 360, &hori, &vert);
-
-    EXPECT_EQ(hori, 50);
-    EXPECT_EQ(vert, 0);
-}
+// TEST(splitValueFromAngleTest, Value1Angle90) {
+//     float hori = 0;
+//     float vert = 0;
+//
+//     Physics::splitValueFromAngle(50, 90, &hori, &vert);
+//
+//     EXPECT_EQ(hori, 12.5);
+//     EXPECT_EQ(vert, 37.5);
+// }
+// TEST(splitValueFromAngleTest, Value1Angle180) {
+//     float hori = 0;
+//     float vert = 0;
+//
+//     Physics::splitValueFromAngle(50, 180, &hori, &vert);
+//
+//     EXPECT_EQ(hori, 25);
+//     EXPECT_EQ(vert, 25);
+// }
+// TEST(splitValueFromAngleTest, Value1Angle270) {
+//     float hori = 0;
+//     float vert = 0;
+//
+//     Physics::splitValueFromAngle(50, 270, &hori, &vert);
+//
+//     EXPECT_EQ(hori, 37.5);
+//     EXPECT_EQ(vert, 12.5);
+// }
+// TEST(splitValueFromAngleTest, Value1Angle360) {
+//     float hori = 0;
+//     float vert = 0;
+//
+//     Physics::splitValueFromAngle(50, 360, &hori, &vert);
+//
+//     EXPECT_EQ(hori, 50);
+//     EXPECT_EQ(vert, 0);
+// }
